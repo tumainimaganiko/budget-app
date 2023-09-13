775d36acd9e9c566e459ec34b92ee018d1887e93
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-13 09:33:26 +0300

    Migration for Devise

--- db/migrate/20230913004529_create_groups.rb
+++ db/migrate/20230913004529_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :icon
-      t.references :author, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230913011741_create_exchanges.rb
+++ db/migrate/20230913011741_create_exchanges.rb
@@ -2,7 +2,7 @@ class CreateExchanges < ActiveRecord::Migration[7.0]
   def change
     create_table :exchanges do |t|
       t.decimal :amount
-      t.references :author, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
