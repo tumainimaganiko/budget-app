ee652041bec7c3f73c696fcf77450e639751697b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-13 14:42:19 +0300

    Update Association

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,3 +1,4 @@
 class Exchange < ApplicationRecord
-  belongs_to :author
+  belongs_to :user
+  has_many :category_exchanges, foreign_key: 'exchange_id'
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
-  belongs_to :author
+  belongs_to :user
+  has_many :category_exchanges, foreign_key: 'group_id'
 end
