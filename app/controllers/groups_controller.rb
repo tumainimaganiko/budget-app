c180080719c71914da238d94ce7d46c5d28e435e
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-14 11:00:22 +0300

    Retrieve Categories for the User

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,15 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @categories = current_user.groups
+    @current_user = current_user
+  end
 
-  def new; end
+  def new
+    @category = Group.new
+    @current_user = current_user
+  end
 
-  def create; end
+  
 end
